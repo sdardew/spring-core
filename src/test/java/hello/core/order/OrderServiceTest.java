dc9947d17732be99ae7094b35dfc1b0236447368
sdardew <sdardew@gmail.com>
2021-11-23 15:47:49 +0900

    Add RateDiscountPolicyTest

--- src/test/java/hello/core/order/OrderServiceTest.java
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -5,6 +5,7 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
